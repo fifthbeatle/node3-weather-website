5932ce5edbcb32aca4b2d064dfb0e61d1c2fb3cd
ARNOLD D SOUZA <arnold.dsouza@in.ibm.com>
2020-01-08 19:59:27 +0530

    Changed the port to 8080 for Openshift

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const forecast = require('./utils/forecast.js')
 // console.log(path.join(__dirname, '../public'))
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 //define paths for Express config
 const viewsPath = path.join(__dirname, '../templates/views')
